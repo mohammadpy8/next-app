075ff68905df3cd166348b66aeac28ee182ca53b
mohammadpy8 <mohammadpy8@gmail.com>
2024-04-16 10:58:22 +0330

    search box

---
+++ app/a/data.ts
@@ -0,0 +1,2 @@
+const dataArray = ["mmm", "ali", "hossein", "hos", "hass", "bbb00", "bbb", "545"];
+export default dataArray;
--- app/a/page.tsx
+++ app/a/page.tsx
@@ -1,18 +1,68 @@
-import { Box } from "@mui/material";
-import { Checkbox, Button } from "@nextui-org/react";
+"use client";
 
-const page = () => {
+import { Box, TextField, Typography } from "@mui/material";
+import { type ChangeEvent, useState } from "react";
+import dataArray from "./data";
+import debounce from "lodash/debounce";
+
+const SearchBox = () => {
+  const [valueInput, setValueInput] = useState<string>("");
+  const [resultSearchBox, setResultSearchBox] = useState<string[]>([]);
+
+  const _changeBoxHandle = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const getValueOfInput = (event.target as HTMLInputElement | HTMLTextAreaElement).value;
+    setValueInput(getValueOfInput);
+    const filterData = dataArray.filter((item: string) =>
+      item.toLowerCase().includes(getValueOfInput.toLowerCase())
+    );
+    console.log("ll", valueInput.length)
+    if (getValueOfInput.length) {
+      setResultSearchBox(filterData);
+    } else {
+      setResultSearchBox([]);
+    }
+    console.log("value==>", getValueOfInput, filterData);
+  };
+
   return (
-    <Box>
-      <Checkbox defaultSelected size="lg">
-        Option
-      </Checkbox>
-      <Button color="primary" variant="bordered">
-        Bordered
-      </Button>
+    <Box width={"100%"} height={"350px"} bgcolor={"#3255"} margin={"20px"}>
+      <Box
+        sx={{
+          padding: "0.8rem",
+          width: "85%",
+        }}
+      >
+        <TextField value={valueInput} onChange={_changeBoxHandle} />
+      </Box>
+      <Box display={"flex"} flexDirection={"row-reverse"} columnGap={"25px"}>
+        <Box
+          component={Typography}
+          sx={{
+            color: "red",
+            fontSize: "25px",
+            fontWeight: "bold",
+          }}
+        >
+          Result :
+        </Box>
+        <Box display={"flex"} flexDirection={"column"}>
+          {resultSearchBox.length > 0 && resultSearchBox?.map((text: string, index) => (
+            <Box
+              key={index}
+              component={Typography}
+              sx={{
+                color: "#00ff",
+                fontSize: "25px",
+                fontWeight: "bold",
+              }}
+            >
+              {text}
+            </Box>
+          ))}
+        </Box>
+      </Box>
     </Box>
   );
 };
 
-
-export default page;
+export default SearchBox;
