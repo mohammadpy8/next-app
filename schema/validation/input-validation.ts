c19618eba4bf9e316cf9ed047baa8447eecca8c1
mohammadpy8 <mohammadpy8@gmail.com>
2024-05-01 14:48:54 +0330

    message of validation

--- schema/validation/input-validation.ts
+++ schema/validation/input-validation.ts
@@ -2,20 +2,29 @@ import { z, type ZodType } from "zod";
 
 const inputSchema: z.infer<ZodType> = z
   .object({
-    email: z.string().email(),
-    age: z.number({ message: "" }).min(1, { message: "" }).max(3, { message: "" }),
-    phoneNumber: z.number().min(1).max(3, { message: "" }),
+    email: z.string({ message: "email required" }).email({ message: "email invalid" }),
+    age: z
+      .number({ message: "should number type value" })
+      .min(1, { message: "have 1 min value age " })
+      .max(3, { message: "have max 3 value age" }),
+    phoneNumber: z
+      .number({ message: "phone number is required" })
+      .min(8, { message: "have min 8 value of phone number" })
+      .max(11, { message: "have max 11 value of phone number" }),
     website: z
-      .string({ message: "" })
-      .url({ message: "" })
-      .min(15, { message: "" })
-      .max(35, { message: "" })
-      .includes(".com", { message: "" }),
-    password: z.string({ message: "" }).min(6, { message: "" }).max(15, { message: "" }),
-    confirmPassword: z.string({ message: "" }),
+      .string({ message: "value of website is required" })
+      .url({ message: "this value is invalid" })
+      .min(15, { message: "have 15 min value of url website" })
+      .max(35, { message: "have 35 max value of url website" })
+      .includes(".com", { message: "have include .com value " }),
+    password: z
+      .string({ message: "password is required" })
+      .min(6, { message: "password 6 min of value" })
+      .max(15, { message: "password 15 max of value" }),
+    confirmPassword: z.string({ message: "confirm password is required" }),
   })
   .refine((data) => data.password === data.confirmPassword, {
-    message: "",
+    message: "confirm password not equal to password",
     path: ["confirmPassword"],
   });
 
