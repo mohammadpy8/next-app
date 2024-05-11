f38eb79396f2ab7de2e63f1cd8bdc8e59fd3cb05
mohammadpy8 <mohammadpy8@gmail.com>
2024-05-11 21:43:48 +0330

    icon component

---
+++ components/custom/iconComponent/IconComponent.tsx
@@ -0,0 +1,32 @@
+import { Box } from "@mui/material";
+import type { FC } from "react";
+import React from "react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      iconName: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+  }
+}
+
+type PropsIconComponent = {
+  iconName: JSX.IntrinsicElements;
+  iconColor: string;
+  iconSize: number | string;
+};
+
+const IconComponent: FC<PropsIconComponent> = ({ iconColor, iconName, iconSize, ...restIcon }) => {
+  const restProps = {
+    ...restIcon,
+    iconSize,
+  };
+
+  return (
+    <Box>
+      <iconName color={iconColor} {...restProps} />
+    </Box>
+  );
+};
+
+export { IconComponent };
