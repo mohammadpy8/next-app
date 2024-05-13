739ac1087b996aa22113f69a8833442ac857e816
mohammadpy8 <mohammadpy8@gmail.com>
2024-05-13 21:01:41 +0330

    close button modal in dialog

--- app/home-page/page.tsx
+++ app/home-page/page.tsx
@@ -11,22 +11,33 @@ const HomePage = () => {
     return target.value;
   };
 
+  //@ state for show and close modal buttons
+
   const [showModal, setShowModal] = useState<boolean>(false);
 
-  // console.log("showModal", showModal);
-
-  // console.log("ggggg")
-
   return (
-    // <Stack>
     <Box>
       {/* <Input placeholderInput="NAME" onChangeInput={() => changeHander} /> */}
       <ModalComponent showModal={showModal} setShowModal={setShowModal}>
-        <Typography>nxdkjhfdivn </Typography>
+        <Box width="450px" height="300px" bgcolor="red">
+          <Typography>nxdkjhfdivn </Typography>
+          <Button
+            onClick={() => setShowModal(!showModal)}
+            sx={{
+              backgroundColor: "#00ff",
+              color: "#fff",
+              textTransform: "initial",
+              "&:hover": {
+                backgroundColor: "#00ff",
+              },
+            }}
+          >
+            close modal
+          </Button>
+        </Box>
       </ModalComponent>
       <Button onClick={() => setShowModal(!showModal)}>show modal</Button>
     </Box>
-    // </Stack>
   );
 };
 
